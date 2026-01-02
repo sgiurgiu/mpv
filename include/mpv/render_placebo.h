b6077956b9097dcecc9876b627725cbe0160d2fc
Sergiu Giurgiu <sgiurgiu11@gmail.com>
2026-01-02 13:22:47 -0500

    Refactored the libmpv libplacebo module, to stop pretending we're gpu-next, we're just libplacebo

---
+++ include/mpv/render_placebo.h
@@ -0,0 +1,91 @@
+/* Copyright (C) 2024 the mpv developers
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef MPV_CLIENT_API_RENDER_LIBPLACEBO_H_
+#define MPV_CLIENT_API_RENDER_LIBPLACEBO_H_
+
+#include "render.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Libplacebo backend
+ * ------------------------------------
+ *
+ * This header contains definitions for using the libplacebo renderer with the
+ * render.h API. The libplacebo renderer uses libplacebo directly and provides
+ * advanced rendering features like HDR tone mapping, frame interpolation,
+ * and high-quality scaling.
+ *
+ * The client is responsible for:
+ * - Creating and managing the graphics context (Vulkan instance/device or
+ *   OpenGL context)
+ * - Creating and providing a pl_swapchain from libplacebo
+ *
+ * Basic usage:
+ *
+ *   1. Create pl_log using mppl_log_create()
+ *   5. Call mpv_render_context_create() with:
+ *      - MPV_RENDER_PARAM_API_TYPE = MPV_RENDER_API_TYPE_LIBPLACEBO
+ *      - MPV_RENDER_PARAM_LIBPLACEBO_SWAPCHAIN = pl_swapchain
+ *   6. Render loop: mpv_render_context_render() then swap buffers
+ */
+ 
+ enum {
+ 
+     /**
+      * Required for mpv_render_context_create().
+      * Type: pl_swapchain (from libplacebo)
+      *
+      * The swapchain that mpv will render to. The client is responsible for
+      * creating this swapchain and calling pl_swapchain_swap_buffers() after
+      * mpv_render_context_render() returns.
+      *
+      * The swapchain must remain valid for the lifetime of the render context.
+      */
+     MPV_RENDER_PARAM_LIBPLACEBO_SWAPCHAIN = 101,
+ 
+     /**
+      * Optional for mpv_render_context_create().
+      * Type: pl_log (from libplacebo)
+      *
+      * A libplacebo log context for receiving log messages. If not provided,
+      * mpv will create its own pl_log instance.
+      */
+     MPV_RENDER_PARAM_LIBPLACEBO_PL_LOG = 102,
+     /**
+      * Optional for mpv_render_context_render().
+      * Type: pl_options (from libplacebo)
+      *
+      * A libplacebo options context for passing rendering options. If not provided,
+      * mpv will use the default options.
+      */
+     MPV_RENDER_PARAM_LIBPLACEBO_OPTIONS = 103,
+
+ };
+
+/**
+ * API type string for libplacebo backend.
+ * Use with MPV_RENDER_PARAM_API_TYPE.
+ */
+ #define MPV_RENDER_API_TYPE_LIBPLACEBO "libplacebo"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
